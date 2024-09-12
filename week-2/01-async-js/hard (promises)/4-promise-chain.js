d8a3f70285caf6b1f78b25d17803ce94b17af476
abhiram-ar <abhiram2001.ars@gmail.com>
2024-09-12 19:29:25 +0530

    promise chain

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,25 @@
  */
 
 function wait1(t) {
-
+    return new Promise(resolve=> setTimeout(resolve, t*1000))
 }
 
 function wait2(t) {
-
+    return new Promise(resolve=> setTimeout(resolve, t*1000))
 }
 
 function wait3(t) {
-
+    return new Promise(resolve=> setTimeout(resolve, t*1000))
 }
 
-function calculateTime(t1, t2, t3) {
-
+ function calculateTime(t1, t2, t3) {
+    return new Promise(resolve => {
+        let time1 = Date.now()
+        wait1(t1).then(()=>wait2(t2)).then(()=> wait3(t3)).then(()=>{
+            let time2= Date.now()
+            return time2-time1;
+        })
+    })
 }
 
 module.exports = calculateTime;
